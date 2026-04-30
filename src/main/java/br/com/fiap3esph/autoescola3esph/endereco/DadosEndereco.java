70e420eff31c520c275df11db1bd92becda60ea1
Laboratório FIAP <labsfiap@fiap.com.br>
2026-04-30 09:52:59 -0300

    add: adicionando rota de put e delete

--- src/main/java/br/com/fiap3esph/autoescola3esph/endereco/DadosEndereco.java
+++ src/main/java/br/com/fiap3esph/autoescola3esph/endereco/DadosEndereco.java
@@ -1,12 +1,28 @@
 package br.com.fiap3esph.autoescola3esph.endereco;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+
 public record DadosEndereco(
+        @NotBlank(message = "Logradouro é obrigatório")
         String logradouro,
+
         String numero,
+
         String complemento,
+
+        @NotBlank(message = "Bairro é obrigatório")
         String bairro,
+
+        @NotBlank(message = "Cidade é obrigatório")
         String cidade,
+
+        @NotBlank(message = "UF obrigatório")
+        @Pattern(regexp = "[A-z]{2}")
         String uf,
+
+        @NotBlank(message = "CEP é obrigatório")
+        @Pattern(regexp = "\\d{5}-\\d{3}")
         String cep
 ) {
 
--- src/main/java/br/com/fiap3esph/autoescola3esph/endereco/Endereco.java
+++ src/main/java/br/com/fiap3esph/autoescola3esph/endereco/Endereco.java
@@ -27,4 +27,28 @@ public class Endereco {
         this.uf = dados.uf();
         this.cep = dados.cep();
     }
+
+    public void atualizarInformacoes(DadosEndereco dados) {
+        if(dados.logradouro() != null && !dados.logradouro().isBlank()){
+            this.logradouro = dados.logradouro();
+        }
+        if(dados.numero() != null){
+            this.numero = dados.numero();
+        }
+        if(dados.complemento() != null){
+            this.complemento = dados.complemento();
+        }
+        if(dados.bairro() != null && !dados.bairro().isBlank()){
+            this.bairro = dados.bairro();
+        }
+        if(dados.cidade() != null && !dados.cidade().isBlank()){
+            this.cidade = dados.cidade();
+        }
+        if(dados.uf() != null && !dados.uf().isBlank()){
+            this.uf = dados.uf();
+        }
+        if(dados.cep() != null && !dados.cep().isBlank()){
+            this.cep = dados.cep();
+        }
+    }
 }
